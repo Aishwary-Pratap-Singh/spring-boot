6351bc03dc325a5ab25778ba0d8a23ecee392fd9
Aishwary-Pratap-Singh <aishwarypsingh@gmail.com>
2022-05-02 11:43:43 +0530

    added delete api

--- src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -82,4 +82,10 @@ public class PostServiceImpl implements PostService {
         return mapToDTO(updatedPost);
 
     }
+
+    @Override
+    public void deletePostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post","id",id));
+        postRepository.delete(post);
+    }
 }
