fa6f6a802ae0b1be5ad986d7bd2dbde446d332c5
Aishwary-Pratap-Singh <aishwarypsingh@gmail.com>
2022-05-03 17:42:08 +0530

    added sorting

--- src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.springbootblogrestapi.repository.PostRepository;
 import com.example.springbootblogrestapi.service.PostService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -61,10 +62,10 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
 
 //        create pageable instance
-        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
 
         Page<Post> posts = postRepository.findAll(pageable);
 //        get content from page object
